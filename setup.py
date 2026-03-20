cce30c5caf7a9595a9e184f7d2f138dbc5806839
Manish Singh <pagalworld9927@gmail.com>
2026-03-20 13:54:32 +0530

    modify requirements and setup.py file

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ HYPHEN_E_DOT = "-e ."
 
 def get_requirements(file_path:str)->List[str]:
     """This function will return list of requirements """
-    with open("file_path") as file_obj:
+    with open(file_path, encoding="utf-8") as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
     
     if HYPHEN_E_DOT in requirements:
         requirements.remove(HYPHEN_E_DOT)
+    return requirements
 
 setup(
     name = "Chatbot",
     version = "0.1.1",
     author = "Manihs singh",
     author_email = "manishmannu2304@gmail.com",
-    long_description = open("README.md").read(),
+    long_description = open("README.md", encoding="utf-8").read(),
     install_requires = get_requirements("requirements.txt")
 )
